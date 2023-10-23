af2ea790953ec315b745d241ca42ecbe5faa5e37
Chumam <chumam@hollywoodbets.net>
2023-10-23 23:03:05 +0200

    Code cleanup

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const firefox = require('selenium-webdriver/firefox');
 const { By, Key, until, WebDriver} = require('selenium-webdriver');
 const { Executor, HttpClient } = require('selenium-webdriver/http');
 
-
 function generateSessionId() {
   return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 }
@@ -18,23 +17,18 @@ app.get('/', (req, res) => {
    res.send('Hello this is the landig page!');
 });
 
-app.get('/OpenGoogle', async(req, res) => {
-  //console.log('one');
+app.get('/openGoogle', async(req, res) => {
   const chromeOptions = new chrome.Options();
     chromeOptions.addArguments('--disable-dev-shm-usage');
     chromeOptions.addArguments('--disable-site-isolation-trials');
     chromeOptions.addArguments('disable-infobars');
     chromeOptions.addArguments('--disable-extensions');
     chromeOptions.addArguments('--disable-gpu');
-  let driver = await  new webdriver.Builder()
+    let driver = await  new webdriver.Builder()
     .forBrowser(webdriver.Browser.CHROME)
     .usingServer('http://localhost:4444/wd/hub')
     .setChromeOptions(chromeOptions)
     .build();
-  
-    // let httpClient = new HttpClient('http://localhost:4444/wd/hub');
-    // let executor = new Executor(httpClient);
-    // let driver2 = await new WebDriver(driver.getSession(),executor);
     
     let sessionId = (await driver.getSession()).getId();
     console.log(sessionId);
@@ -43,41 +37,33 @@ app.get('/OpenGoogle', async(req, res) => {
         await driver.get('https://www.google.com/ncr');
         res.send(sessionId);
       } finally {
-       // await driver.quit();
       }
-
 });
 
-app.get('/ReuseSession/:sessionId', async (req, res) => {
+app.get('/searchGoogle/:sessionId', async (req, res) => {
   const { sessionId } = req.params;
-  console.log("Incoming_ID__"+sessionId);
-
-    
-
 
       let httpClient = new HttpClient('http://localhost:4444/wd/hub');
       let executor = new Executor(httpClient);
       let driver2 = await new WebDriver(sessionId,executor);
-      console.log(driver2);
-     // let sessionIdTest= (await driver22.getSession()).getId();
-      //console.log(sessionIdTest);
-    // Perform actions with the new driver instance (driver2)
     try {
       await driver2.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-      res.send(`Session Data for ID ${sessionId}: ${sessionData} (Driver2 created and navigated to a new page)`);
-     // await driver2.wait(until.titleIs('webdriver - Google Search'), 1000);
+      await delay(3000);
+
+      res.send(`Session Data for ID ${sessionId}`);
     }catch(err){
         console.log(err);
         driver2.quit();
     } finally {
       await driver2.quit();
     }
-
-    
-  // } else {
-  //   res.status(404).send('Session ID not found');
-  // }
 });
+
+async function delay(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
